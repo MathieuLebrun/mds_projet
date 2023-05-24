8e0fa2ff9296526710ef0ebaf9b4f693564a7f70
dede <teo.garbarino@etu.unice.fr>
2023-05-24 19:55:59 +0200

    Front/MainScreen

--- mds_project/lib/mainScreen.dart
+++ mds_project/lib/mainScreen.dart
@@ -1,10 +1,16 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:mds_project/services/api_service.dart';
 
 import 'package:mds_project/services/persistancehandler.dart';
 import 'package:mds_project/views/questionnaire.dart';
 
+import 'login.dart';
+import 'models/params_requets_model.dart';
 import 'views/coursScreen.dart';
 import 'views/draganddropScreen.dart';
+import 'utilities/global.dart';
 
 class mainScreen extends StatefulWidget {
   final Function(int) onChangedStep;
@@ -17,9 +23,11 @@ class mainScreen extends StatefulWidget {
 class _mainScreenState extends State<mainScreen> {
   late String token;
   String currentMatiere = "Math";
+  TextEditingController emailController = TextEditingController();
+  TextEditingController usernameController = TextEditingController();
   int selectedIndex = 0;
   Color currentcolors = const Color(0xFFDD45F2);
-
+  Color currentcolorsMain = const Color(0xFFDD45F2);
   @override
   void initState() {
     // ? La méthode init state c'est une méthode lancer une seule fois quand on arriver dans une view donc au lancement dans l'app on va dans le gestionnaire puis cette methode utiliser qu'une fois
@@ -29,6 +37,8 @@ class _mainScreenState extends State<mainScreen> {
 
   init() async {
     token = (await PersistanceHandler().getTokenEDP())!;
+    usernameController.text = (await PersistanceHandler().getUsername())!;
+    emailController.text = (await PersistanceHandler().getEmail())!;
   }
 
   @override
@@ -36,27 +46,6 @@ class _mainScreenState extends State<mainScreen> {
     var largeurEcran = MediaQuery.of(context).size.width;
     var hauteurEcran = MediaQuery.of(context).size.height;
     var fractionEcran = 8;
-    const TextStyle optionStyle =
-        TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-    const List<Widget> _widgetOptions = <Widget>[
-      Text(
-        'Index 0: Home',
-        style: optionStyle,
-      ),
-      Text(
-        'Index 1: Business',
-        style: optionStyle,
-      ),
-      Text(
-        'Index 2: School',
-        style: optionStyle,
-      ),
-      Text(
-        'Index 3: Settings',
-        style: optionStyle,
-      ),
-    ];
-
     var widthFactorImg = 0.22;
     var heightFactorImg = 0.8;
     return Scaffold(
@@ -100,49 +89,57 @@ class _mainScreenState extends State<mainScreen> {
           }),
         ],
       ),
-      backgroundColor: Colors.black,
+      backgroundColor: currentcolorsMain,
       body: ListView(
         scrollDirection: Axis.vertical,
         children: <Widget>[
-          GestureDetector(
+          InkWell(
             onTap: () {
               Navigator.push(context,
                   MaterialPageRoute(builder: (context) => coursScreen()));
             },
             child: Container(
-              color: Colors.white,
+              color: currentcolorsMain,
               alignment: Alignment(0, -0),
               height: (hauteurEcran / fractionEcran),
               width: 250,
-              child: monWidget(widthFactorImg, heightFactorImg, "test"),
+              child: monWidget(widthFactorImg, heightFactorImg, "Logo1etoile"),
             ),
           ),
           GestureDetector(
             onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => draganddropScreen()));
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => QuestionnairePage(
+                            questions: Math1,
+                          )));
             },
             child: Container(
               height: (hauteurEcran / fractionEcran),
-              color: Colors.black,
+              color: currentcolorsMain,
               alignment: Alignment(-0.3, -0),
-              child: monWidget(widthFactorImg, heightFactorImg, "testcarr"),
+              child: monWidget(widthFactorImg, heightFactorImg, "Logo2etoile"),
             ),
           ),
           GestureDetector(
             onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => QuestionnairePage()));
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => QuestionnairePage(
+                            questions: Math2,
+                          )));
             },
             child: Container(
               height: (hauteurEcran / fractionEcran),
-              color: Colors.black,
+              color: currentcolorsMain,
               alignment: Alignment(-0.5, -0),
-              child: monWidget(widthFactorImg, heightFactorImg, "testcarr"),
+              child: monWidget(widthFactorImg, heightFactorImg, "Logo3etoile"),
             ),
           ),
           Container(
-            color: Colors.black,
+            color: currentcolorsMain,
             height: (hauteurEcran / fractionEcran),
             width: 250,
             child: Row(
@@ -157,17 +154,19 @@ class _mainScreenState extends State<mainScreen> {
                       Navigator.push(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => QuestionnairePage()));
+                              builder: (context) => QuestionnairePage(
+                                    questions: Math2,
+                                  )));
                     },
                     child: Container(
-                      child: monWidget(1.0, heightFactorImg, "testcarr"),
+                      child: monWidget(1.0, heightFactorImg, "LogoBoss1etoile"),
                     ),
                   ),
                 ),
                 Expanded(
                   child: Container(
-                    child: monWidgetGIF(1.0, 4.0, "gifdanseshelou"),
-                  ),
+                      //  child: monWidgetGIF(1.0, 4.0, "gifdanseshelou"),
+                      ),
                 ),
                 SizedBox(
                   width: 20,
@@ -176,26 +175,26 @@ class _mainScreenState extends State<mainScreen> {
             ),
           ),
           Container(
-            color: Colors.black,
+            color: currentcolorsMain,
             alignment: Alignment(0, -0),
             height: (hauteurEcran / fractionEcran),
             width: 250,
-            child: monWidget(widthFactorImg, heightFactorImg, "test"),
+            child: monWidget(widthFactorImg, heightFactorImg, "Logo1etoile"),
           ),
           Container(
             height: (hauteurEcran / fractionEcran),
-            color: Colors.black,
+            color: currentcolorsMain,
             alignment: Alignment(0.3, -0),
-            child: monWidget(widthFactorImg, heightFactorImg, "testcarr"),
+            child: monWidget(widthFactorImg, heightFactorImg, "Logo2etoile"),
           ),
           Container(
             height: (hauteurEcran / fractionEcran),
-            color: Colors.black,
+            color: currentcolorsMain,
             alignment: Alignment(0.5, -0),
-            child: monWidget(widthFactorImg, heightFactorImg, "testcarr"),
+            child: monWidget(widthFactorImg, heightFactorImg, "Logo3etoile"),
           ),
           Container(
-            color: Colors.black,
+            color: currentcolorsMain,
             height: (hauteurEcran / fractionEcran),
             width: 250,
             child: Row(
@@ -206,12 +205,12 @@ class _mainScreenState extends State<mainScreen> {
                 ),
                 Expanded(
                   child: Container(
-                    child: monWidgetGIF2(1.0, 4.0, "gifdanseshelou2"),
-                  ),
+                      //   child: monWidgetGIF2(1.0, 4.0, "gifdanseshelou2"),
+                      ),
                 ),
                 Expanded(
                   child: Container(
-                    child: monWidget(1.0, heightFactorImg, "testcarr"),
+                    child: monWidget(1.0, heightFactorImg, "LogoBoss2etoile"),
                   ),
                 ),
                 SizedBox(
@@ -221,11 +220,11 @@ class _mainScreenState extends State<mainScreen> {
             ),
           ),
           Container(
-            color: Colors.black,
+            color: currentcolorsMain,
             alignment: Alignment(0, -0),
             height: (hauteurEcran / fractionEcran),
             width: 250,
-            child: monWidget(widthFactorImg, heightFactorImg, "test"),
+            child: monWidget(widthFactorImg, heightFactorImg, "Logo1etoile"),
           ),
         ],
       ),
@@ -237,18 +236,18 @@ class _mainScreenState extends State<mainScreen> {
             backgroundColor: Color(0xFFDD45F2),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
+            icon: Icon(Icons.settings),
+            label: 'settings',
             backgroundColor: Color(0xFFFFFF75),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-            backgroundColor: Color.fromARGB(255, 252, 68, 48),
+            icon: Icon(Icons.trending_up),
+            label: 'Statistique',
+            backgroundColor: Color(0xFFc93626),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
+            icon: Icon(Icons.logout),
+            label: 'logout',
             backgroundColor: Color(0xFF383838),
           ),
         ],
@@ -266,23 +265,38 @@ class _mainScreenState extends State<mainScreen> {
         case 0:
           {
             currentcolors = const Color(0xFFDD45F2);
+            currentcolorsMain = const Color(0xFFff48fc);
           }
           break;
-
         case 1:
           {
             currentcolors = const Color(0xFFFFFF75);
+            currentcolorsMain = const Color(0xFFffffb9);
+            //Inserer le logout
+
+            showDialogFunction(context);
           }
           break;
 
         case 2:
           {
-            currentcolors = const Color.fromARGB(255, 252, 68, 48);
+            currentcolors = const Color(0xFFc93626);
+            currentcolorsMain = const Color.fromARGB(255, 252, 68, 28);
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => draganddropScreen()));
           }
           break;
         case 3:
           {
             currentcolors = const Color(0xFF383838);
+            currentcolorsMain = const Color(0xFF848484);
+            PersistanceHandler().clearPersistentData();
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => LoginScreen(
+                          onChangedStep: (int, String, String2) {},
+                        )));
           }
           break;
 
@@ -297,16 +311,91 @@ class _mainScreenState extends State<mainScreen> {
 
   Widget monWidget(widthFactor, heightFactor, pathimg) {
     return FractionallySizedBox(
-      widthFactor: widthFactor,
-      heightFactor: heightFactor,
-      child: GestureDetector(
-        onTap: () {},
-        child: Container(
-          child: Image(
-            image: AssetImage("assets/" + pathimg + ".jpg"),
-          ),
-        ),
+      widthFactor: widthFactor * 1.35,
+      heightFactor: heightFactor * 1.35,
+      child: Image(
+        image: AssetImage("assets/images/" + pathimg + ".png"),
       ),
+    );
+  }
+
+  void showDialogFunction(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Center(
+          child: AlertDialog(
+            content: SizedBox(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height * 0.5,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Modifications de vos paramètres',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Email',
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                  ),
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                    ),
+                  ),
+                  Text(
+                    'Username',
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                  ),
+                  TextField(
+                    controller: usernameController,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 200.0,
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        await _ModifParams();
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text(
+                        'Enregistrer',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: 200,
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        Navigator.of(context).pop();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                      ),
+                      child: const Text(
+                        'Supprimer votre compte',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 
@@ -314,16 +403,41 @@ class _mainScreenState extends State<mainScreen> {
     return FractionallySizedBox(
       widthFactor: widthFactor,
       heightFactor: heightFactor,
-      child: GestureDetector(
-        onTap: () {},
-        child: Container(
-          alignment: Alignment(0, -1),
-          child: Image(
-            image: AssetImage("assets/" + pathimg + ".gif"),
-          ),
+      child: Container(
+        alignment: Alignment(0, -1),
+        child: Image(
+          image: AssetImage("assets/" + pathimg + ".gif"),
         ),
       ),
     );
+  }
+
+  Future<String> _ModifParams() async {
+    var usernames = await PersistanceHandler().getUsername();
+    var email = await PersistanceHandler().getEmail();
+
+    ParamsModel model = ParamsModel(
+      username: usernameController.text,
+      email: emailController.text,
+    );
+
+    var response = await APIService().updateUserParams(usernames!, model);
+
+    return response;
+  }
+
+  Future<String> _delUser() async {
+    var usernames = await PersistanceHandler().getUsername();
+    var email = await PersistanceHandler().getEmail();
+
+    ParamsModel model = ParamsModel(
+      username: usernameController.text,
+      email: emailController.text,
+    );
+
+    var response = await APIService().updateUserParams(usernames!, model);
+
+    return response;
   }
 
   Widget monWidgetGIF2(widthFactor, heightFactor, pathimg) {
