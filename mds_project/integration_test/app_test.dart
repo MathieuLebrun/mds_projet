85c6872d742c7e5701ce449596cd65f435f305a5
dede <teo.garbarino@etu.unice.fr>
2023-05-24 19:55:29 +0200

    Test/Test

---
+++ mds_project/integration_test/app_test.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:mds_project/main.dart';
+import 'package:mds_project/mainScreen.Dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  group('end-to-end test', () {
+    testWidgets('Login bon', (WidgetTester tester) async {
+      await tester.pumpWidget(const MyApp());
+      await tester.pumpAndSettle();
+      final textFieldFinder1 = find.byKey(Key('email'));
+      final textFieldFinder = find.byKey(Key('mdp'));
+      await tester.enterText(textFieldFinder1, 'teo@garbarinoo.co');
+      await tester.enterText(textFieldFinder, '1234567');
+      final buttonFinder = find.byKey(Key('login'));
+      await tester.tap(buttonFinder);
+    });
+
+    testWidgets('Login faux', (WidgetTester tester) async {
+      await tester.pumpWidget(const MyApp());
+      await tester.pumpAndSettle();
+      final textFieldFinder1 = find.byKey(Key('email'));
+      final textFieldFinder = find.byKey(Key('mdp'));
+      await tester.enterText(textFieldFinder1, 'teo@garbarinoo.co');
+      await tester.enterText(textFieldFinder, '12367');
+      final buttonFinder = find.byKey(Key('login'));
+      await tester.tap(buttonFinder);
+    });
+  });
+}
---
+++ mds_project/integration_test/test_popup.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:mds_project/main.dart';
+import 'package:mds_project/mainScreen.Dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  group('end-to-end test', () {
+    testWidgets('Test popUp', (WidgetTester tester) async {
+      await tester.pumpWidget(const MyApp());
+      await tester.pumpAndSettle();
+      final bottomNavigationBarFinder = find.byType(BottomNavigationBar);
+
+      // Cliquez sur le troisième élément (indice 2)
+      await tester.tap(find.byIcon(Icons.settings).last);
+
+      // Attendez que la transition se termine
+      await tester.pumpAndSettle();
+    });
+  });
+}
