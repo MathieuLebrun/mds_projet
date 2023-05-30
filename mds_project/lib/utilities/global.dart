05c8107948691d2cca205883487f875f3daef8a2
dede <teo.garbarino@etu.unice.fr>
2023-05-30 23:44:15 +0200

    Enhance/globalQuestionnaire
    Ajout des differents questionnaire

--- mds_project/lib/utilities/global.dart
+++ mds_project/lib/utilities/global.dart
@@ -74,3 +74,169 @@ final List<Map<String, Object>> Math2 = [
     ],
   },
 ];
+final List<Map<String, Object>> Math3 = [
+  {
+    'questionText': 'Quelle est la formule du volume d\'un cylindre ?',
+    'answers': [
+      {'text': 'V = π x r² x h', 'score': 1},
+      {'text': 'V = l x L x h', 'score': 0},
+      {'text': 'V = c³', 'score': 0},
+      {'text': 'V = A x h', 'score': 0},
+    ],
+  },
+  {
+    'questionText': 'Quelle est la formule de l\'aire d\'un cercle ?',
+    'answers': [
+      {'text': 'A = π x r²', 'score': 1},
+      {'text': 'A = l x L', 'score': 0},
+      {'text': 'A = c x c', 'score': 0},
+      {'text': 'A = l x h', 'score': 0},
+    ],
+  },
+  {
+    'questionText': 'Quelle est la formule du périmètre d\'un cercle ?',
+    'answers': [
+      {'text': 'P = 2 x π x r', 'score': 1},
+      {'text': 'P = l + L', 'score': 0},
+      {'text': 'P = c x c', 'score': 0},
+      {'text': 'P = l + h + L', 'score': 0},
+    ],
+  },
+];
+
+final List<Map<String, Object>> Francais1 = [
+  {
+    'questionText': 'Quelle est la définition du mot "éloquent" ?',
+    'answers': [
+      {'text': 'Qui parle avec assurance', 'score': 1},
+      {'text': 'Qui est silencieux', 'score': 0},
+      {'text': 'Qui est ennuyeux', 'score': 0},
+      {'text': 'Qui est maladroit', 'score': 0},
+    ],
+  },
+  {
+    'questionText': '''Quel est l'antonyme du mot "brillant" ?''',
+    'answers': [
+      {'text': 'Sombre', 'score': 0},
+      {'text': 'Stupide', 'score': 0},
+      {'text': 'Terne', 'score': 1},
+      {'text': 'Faible', 'score': 0},
+    ],
+  },
+  {
+    'questionText':
+        '''Quelle est la règle d'accord du participe passé avec l'auxiliaire "avoir" ?''',
+    'answers': [
+      {'text': '''Il s'accorde avec le sujet''', 'score': 0},
+      {
+        'text': '''Il s'accorde avec le complément d'objet direct''',
+        'score': 0
+      },
+      {'text': '''Il ne s'accorde pas''', 'score': 1},
+      {'text': '''Il s'accorde avec le verbe auxiliaire''', 'score': 0},
+    ],
+  },
+];
+
+final List<Map<String, Object>> Francais2 = [
+  {
+    'questionText': '''Qu'est-ce qu'un oxymore en littérature ?''',
+    'answers': [
+      {
+        'text': 'Une figure de style qui associe deux termes contradictoires',
+        'score': 1
+      },
+      {'text': 'Un personnage principal', 'score': 0},
+      {'text': 'Un genre littéraire', 'score': 0},
+      {'text': 'Un procédé narratif', 'score': 0},
+    ],
+  },
+];
+
+final List<Map<String, Object>> Anglais1 = [
+  {
+    'questionText': 'What is the plural of "child"?',
+    'answers': [
+      {'text': 'Childs', 'score': 0},
+      {'text': 'Children', 'score': 1},
+      {'text': 'Childes', 'score': 0},
+      {'text': 'Childies', 'score': 0},
+    ],
+  },
+  {
+    'questionText': 'What is the past tense of "go"?',
+    'answers': [
+      {'text': 'Went', 'score': 1},
+      {'text': 'Gone', 'score': 0},
+      {'text': 'Goed', 'score': 0},
+      {'text': 'Goes', 'score': 0},
+    ],
+  },
+  {
+    'questionText': 'Which one is the correct sentence?',
+    'answers': [
+      {'text': '''She don't like apples.''', 'score': 0},
+      {'text': '''She doesn't likes apples.''', 'score': 0},
+      {'text': '''She doesn't like apples.''', 'score': 1},
+      {'text': '''She don't likes apples.''', 'score': 0},
+    ],
+  },
+];
+final List<Map<String, Object>> Anglais2 = [
+  {
+    'questionText': 'What is the plural of "child"?',
+    'answers': [
+      {'text': 'Childs', 'score': 0},
+      {'text': 'Children', 'score': 1},
+      {'text': 'Childes', 'score': 0},
+      {'text': 'Childies', 'score': 0},
+    ],
+  },
+  {
+    'questionText': 'What is the past tense of "go"?',
+    'answers': [
+      {'text': 'Went', 'score': 1},
+      {'text': 'Gone', 'score': 0},
+      {'text': 'Goed', 'score': 0},
+      {'text': 'Goes', 'score': 0},
+    ],
+  },
+  {
+    'questionText': 'Which one is the correct sentence?',
+    'answers': [
+      {'text': '''She don't like apples.''', 'score': 0},
+      {'text': '''She doesn't likes apples.''', 'score': 0},
+      {'text': '''She doesn't like apples.''', 'score': 1},
+      {'text': '''She don't likes apples.''', 'score': 0},
+    ],
+  },
+];
+final List<Map<String, Object>> Anglais3 = [
+  {
+    'questionText': 'What is the plural of "child"?',
+    'answers': [
+      {'text': 'Childs', 'score': 0},
+      {'text': 'Children', 'score': 1},
+      {'text': 'Childes', 'score': 0},
+      {'text': 'Childies', 'score': 0},
+    ],
+  },
+  {
+    'questionText': 'What is the past tense of "go"?',
+    'answers': [
+      {'text': 'Went', 'score': 1},
+      {'text': 'Gone', 'score': 0},
+      {'text': 'Goed', 'score': 0},
+      {'text': 'Goes', 'score': 0},
+    ],
+  },
+  {
+    'questionText': 'Which one is the correct sentence?',
+    'answers': [
+      {'text': '''She don't like apples.''', 'score': 0},
+      {'text': '''She doesn't likes apples.''', 'score': 0},
+      {'text': '''She doesn't like apples.''', 'score': 1},
+      {'text': '''She don't likes apples.''', 'score': 0},
+    ],
+  },
+];
